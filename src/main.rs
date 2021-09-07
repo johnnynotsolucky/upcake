f77f24d8a8a02921b46f40a8fb9bf7eb6a5bf9ce
Tyrone Tudehope <tyronetudehope@gmail.com>
2021-09-08 00:03:42 +0200

    Use jql for path assertions

--- src/main.rs
+++ src/main.rs
@@ -1,134 +1,149 @@
 use eyre::Result;
-use httpstat::{httpstat, Config as HttpstatConfig, StatResult};
+use httpstat::{httpstat, Config as HttpstatConfig, Header, StatResult as HttpstatResult, Timing};
 use serde::{Deserialize, Serialize};
-use serde_yaml::Value;
-use std::fs;
+use serde_json::Value as JsonValue;
+use serde_yaml::Value as YamlValue;
 use std::time::Duration;
+use std::{fs, str};
 use structopt::StructOpt;
 
-#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+#[derive(Deserialize, Default, Debug, Clone)]
 pub struct Config {
     pub request: String,
     pub data: Option<String>,
     pub headers: Option<Vec<String>>,
     pub url: String,
-    pub assertions: Option<Value>,
+    pub assertions: Vec<Assertion>,
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct RequestAssertion<T> {
+#[derive(Deserialize, Debug, Clone)]
+pub struct RequestAssertion<T: ?Sized> {
+    #[serde(default)]
+    path: String,
     #[serde(flatten)]
     assertion: T,
 }
 
 trait Assert {
-    fn assert(&self, result: &Value, path: String, callback: AssertCallback) -> Result<()>;
+    fn assert(&self, result: &YamlValue) -> bool;
 }
 
-#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+#[derive(Deserialize, Default, Debug, Clone)]
 pub struct WithinRange {
-    pub min: Value,
-    pub max: Value,
+    pub min: YamlValue,
+    pub max: YamlValue,
 }
 
-fn get_field_value(value: &Value, field: String) -> Option<Value> {
-    let mut result = value.clone();
-    let path = field.split('.');
-    for section in path {
-        if let Some(nested) = result.get(section) {
-            result = nested.clone();
-        } else {
-            return None;
-        }
-    }
-
-    Some(result)
-}
-
-fn value_to_string(value: &Value) -> String {
+fn value_to_string(value: &YamlValue) -> String {
     match value {
-        Value::Null => "null".into(),
-        Value::Bool(inner) => format!("{}", inner),
-        Value::Number(inner) => {
+        YamlValue::Null => "null".into(),
+        YamlValue::Bool(inner) => format!("{}", inner),
+        YamlValue::Number(inner) => {
             if inner.is_i64() {
                 format!("{}", inner.as_i64().unwrap())
             } else {
                 format!("{}", inner.as_f64().unwrap())
             }
         }
-        Value::String(inner) => inner.into(),
-        Value::Sequence(inner) => serde_yaml::to_string(inner).unwrap(),
-        Value::Mapping(inner) => serde_yaml::to_string(inner).unwrap(),
+        YamlValue::String(inner) => inner.into(),
+        YamlValue::Sequence(inner) => serde_json::to_string(inner).unwrap(),
+        YamlValue::Mapping(inner) => serde_json::to_string(inner).unwrap(),
     }
 }
 
 impl Assert for RequestAssertion<WithinRange> {
-    fn assert(&self, value: &Value, path: String, callback: AssertCallback) -> Result<()> {
-        let min = &self.assertion.min;
-        let max = &self.assertion.max;
-        if *value < *min || *value > *max {
-            let message = format!(
-                "{} not within range of [{}, {}]",
-                value_to_string(value),
-                value_to_string(min),
-                value_to_string(max)
-            );
-
-            callback(AssertionResult::Failure(path, message));
-        } else {
-            callback(AssertionResult::Success);
-        }
-
-        Ok(())
+    fn assert(&self, value: &YamlValue) -> bool {
+        *value >= self.assertion.min && *value <= self.assertion.max
+        // let message = format!(
+        // 	"{} not within range of [{}, {}]",
+        // 	value_to_string(value),
+        // 	value_to_string(min),
+        // 	value_to_string(max)
+        // );
+        //
+        // callback(AssertionResult::Failure(
+        // 	Assertion::WithinRange(self.clone()),
+        // 	message,
+        // ));
     }
 }
 
-#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+#[derive(Deserialize, Default, Debug, Clone)]
 pub struct Equal {
-    pub value: Value,
+    pub value: YamlValue,
 }
 
 impl Assert for RequestAssertion<Equal> {
-    fn assert(&self, value: &Value, path: String, callback: AssertCallback) -> Result<()> {
-        let expected_value = &self.assertion.value;
-        if *value != *expected_value {
-            let message = format!(
-                "Expected {} to equal {}, found {}",
-                path,
-                value_to_string(expected_value),
-                value_to_string(value)
-            );
-
-            callback(AssertionResult::Failure(path, message));
-        } else {
-            callback(AssertionResult::Success);
-        }
-
-        Ok(())
+    fn assert(&self, value: &YamlValue) -> bool {
+        *value == self.assertion.value
     }
 }
 
-#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone)]
+pub struct Length {
+    pub length: Box<Assertion>,
+}
+
+impl Assert for RequestAssertion<Length> {
+    fn assert(&self, value: &YamlValue) -> bool {
+        let length = match value {
+            YamlValue::String(value) => Some(value.len()),
+            YamlValue::Mapping(value) => Some(value.len()),
+            YamlValue::Sequence(value) => Some(value.len()),
+            _ => None,
+        };
+
+        length.map_or(false, |length| {
+            self.assertion.length.inner_assert(&YamlValue::from(length))
+        })
+    }
+}
+
+#[derive(Deserialize, Default, Debug, Clone)]
 pub struct NotEqual {
-    pub value: Value,
+    pub value: YamlValue,
 }
 
 impl Assert for RequestAssertion<NotEqual> {
-    fn assert(&self, value: &Value, path: String, callback: AssertCallback) -> Result<()> {
-        let expected_value = &self.assertion.value;
-        if *value == *expected_value {
-            let message = format!("Expected {} to not equal {}", path, value_to_string(value));
-
-            callback(AssertionResult::Failure(path, message));
-        } else {
-            callback(AssertionResult::Success);
-        }
-
-        Ok(())
+    fn assert(&self, value: &YamlValue) -> bool {
+        *value != self.assertion.value
     }
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Deserialize, Default, Debug, Clone)]
+pub struct Contains {
+    pub value: YamlValue,
+}
+
+impl Assert for RequestAssertion<Contains> {
+    fn assert(&self, value: &YamlValue) -> bool {
+        match value {
+            YamlValue::String(value) => self
+                .assertion
+                .value
+                .as_str()
+                .map_or(false, |substring| value.contains(substring)),
+            YamlValue::Mapping(value) => self
+                .assertion
+                .value
+                .as_str()
+                .map_or(false, |key| value.contains_key(&YamlValue::from(key))),
+            YamlValue::Sequence(value) => self.assertion.value.as_i64().map_or(false, |idx| {
+                // serde_yaml::Value only converts to i64, we'd like to not panic if the assertion
+                // uses something less than 0.
+                // TODO at somepoint we should validate the inputs
+                if idx >= 0 {
+                    value.len() >= idx as usize
+                } else {
+                    false
+                }
+            }),
+            _ => false,
+        }
+    }
+}
+
+#[derive(Deserialize, Debug, Clone)]
 #[serde(tag = "__type")]
 pub enum Assertion {
     #[serde(rename = "within-range")]
@@ -137,35 +152,114 @@ pub enum Assertion {
     Equal(RequestAssertion<Equal>),
     #[serde(rename = "not-equal")]
     NotEqual(RequestAssertion<NotEqual>),
+    #[serde(rename = "length")]
+    Length(RequestAssertion<Length>),
+    #[serde(rename = "contains")]
+    Contains(RequestAssertion<Contains>),
 }
 
 type AssertCallback = fn(AssertionResult);
 
 #[derive(Debug, Clone)]
 pub enum AssertionResult {
-    Success,
-    Failure(String, String),
+    Success(Assertion, JsonValue),
+    Failure(Assertion, Option<JsonValue>, String),
 }
 
 impl Assertion {
-    fn assert(&self, result: &StatResult, path: String, callback: AssertCallback) -> Result<()> {
-        let result_value = serde_yaml::to_value(result)?;
-        let field_value = get_field_value(&result_value, path.clone());
-        if path.is_empty() || field_value.is_some() {
-            let value = match field_value {
-                Some(value) => value,
-                None => result_value,
-            };
+    fn assert(&self, value: &JsonValue, callback: AssertCallback) -> Result<()> {
+        if let Ok(found_value) = jql::walker(value, Some(self.path())) {
+            let result = serde_yaml::to_value(&found_value)?;
+            let assertion_result = self.inner_assert(&result);
 
-            match &self {
-                Self::WithinRange(assertion) => assertion.assert(&value, path, callback),
-                Self::Equal(assertion) => assertion.assert(&value, path, callback),
-                Self::NotEqual(assertion) => assertion.assert(&value, path, callback),
+            if assertion_result {
+                callback(AssertionResult::Success(self.clone(), found_value));
+            } else {
+                // TODO implement Display for AssertionResult
+                callback(AssertionResult::Failure(
+                    self.clone(),
+                    Some(found_value),
+                    format!("{} failed for reasons", &self.path()),
+                ));
             }
         } else {
-            callback(AssertionResult::Failure(path, "Invalid path".into()));
-            Ok(())
+            callback(AssertionResult::Failure(
+                self.clone(),
+                None,
+                format!("{} is invalid", &self.path()),
+            ));
         }
+
+        Ok(())
+    }
+
+    fn path(&self) -> &str {
+        match &self {
+            Self::WithinRange(assertion) => &assertion.path,
+            Self::Equal(assertion) => &assertion.path,
+            Self::NotEqual(assertion) => &assertion.path,
+            Self::Length(assertion) => &assertion.path,
+            Self::Contains(assertion) => &assertion.path,
+        }
+    }
+
+    fn inner_assert(&self, value: &YamlValue) -> bool {
+        match &self {
+            Self::WithinRange(assertion) => assertion.assert(value),
+            Self::Equal(assertion) => assertion.assert(value),
+            Self::NotEqual(assertion) => assertion.assert(value),
+            Self::Length(assertion) => assertion.assert(value),
+            Self::Contains(assertion) => assertion.assert(value),
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct StatResult {
+    pub http_version: String,
+    pub response_code: i32,
+    pub response_message: Option<String>,
+    pub headers: Vec<Header>,
+    pub timing: Timing,
+    pub json: Option<JsonValue>,
+    pub body: Option<String>,
+}
+
+impl From<HttpstatResult> for StatResult {
+    fn from(result: HttpstatResult) -> Self {
+        let mut stat_result = Self {
+            http_version: result.http_version,
+            response_code: result.response_code,
+            response_message: result.response_message,
+            headers: result.headers,
+            timing: result.timing,
+            json: None,
+            body: None,
+        };
+
+        if let Ok(body) = str::from_utf8(&result.body[..]) {
+            let mut in_content = false;
+            let lines: Vec<&str> = body
+                .lines()
+                .filter(|line| {
+                    if line.is_empty() && !in_content {
+                        in_content = true;
+                        return false; // Don't include this line though.
+                    }
+
+                    in_content
+                })
+                .collect();
+            let content = lines.join("\n");
+
+            if let Ok(json_content) = serde_json::from_str(&content) {
+                stat_result.json = json_content;
+            }
+
+            stat_result.body = content.into();
+        }
+
+        stat_result
     }
 }
 
@@ -192,74 +286,9 @@ struct Opt {
     upcakefile: String,
 }
 
-fn traverse(
-    value: Value,
-    path: String,
-    current_idx: String,
-    in_sequence: bool,
-    assertions_map: &mut Vec<(String, Assertion)>,
-) -> Result<()> {
-    let path_parts = vec![path.clone(), current_idx];
-    let path_iter = path_parts
-        .iter()
-        .filter(|s| !s.is_empty())
-        .into_iter()
-        .cloned();
-    let next_path = path_iter.collect::<Vec<String>>().join(".");
-
-    let value_result: Result<Assertion, serde_yaml::Error> = serde_yaml::from_value(value.clone());
-
-    // 1. First check whether the Value is an instance of Assertion
-    match value_result {
-        Ok(assertion) => {
-            // If we're currently iterating over a sequence, we don't want to add the index for
-            // each assertion instance to the path.
-            if in_sequence {
-                assertions_map.push((path, assertion));
-            } else {
-                assertions_map.push((next_path, assertion));
-            }
-        }
-        _ => {
-            // 2. Otherwise if it is a sequence or mapping, recursively iterate over its entries
-            match value {
-                Value::Mapping(v) => {
-                    for (k, v1) in v.iter() {
-                        // Currently only bothered with string keys
-                        if let Some(idx) = k.as_str() {
-                            traverse(
-                                v1.clone(),
-                                next_path.clone(),
-                                idx.into(),
-                                false,
-                                assertions_map,
-                            )?;
-                        }
-                    }
-                }
-                Value::Sequence(v) => {
-                    for (idx, v1) in v.iter().enumerate() {
-                        traverse(
-                            v1.clone(),
-                            next_path.clone(),
-                            format!("{}", idx),
-                            true,
-                            assertions_map,
-                        )?;
-                    }
-                }
-                _ => { /* We don't care for this */ }
-            }
-        }
-    }
-
-    Ok(())
-}
-
 fn main() -> Result<()> {
     let opt = Opt::from_args();
 
-    // let upcake_config: Config = serde_yaml::from_str(&fs::read_to_string(opt.upcakefile)?)?;
     let upcake_config: Config = serde_yaml::from_str(&fs::read_to_string(opt.upcakefile)?)?;
 
     let httpstat_config = HttpstatConfig {
@@ -274,25 +303,12 @@ fn main() -> Result<()> {
         url: upcake_config.url,
     };
 
-    let stat_result = httpstat(httpstat_config)?;
+    let stat_result: StatResult = httpstat(httpstat_config)?.into();
 
-    if let Some(assertion) = upcake_config.assertions {
-        let mut assertions: Vec<(String, Assertion)> = Vec::new();
-        traverse(
-            assertion.clone(),
-            "".into(),
-            "".into(),
-            assertion.is_sequence(),
-            &mut assertions,
-        )?;
+    let json_result = serde_json::to_value(&stat_result)?;
 
-        for (path, assertion) in assertions.iter() {
-            assertion.assert(&stat_result, path.clone(), |result| {
-                println!("Result: {:?}", result)
-            })?;
-        }
-    } else {
-        // Can't assert!
+    for assertion in upcake_config.assertions.iter() {
+        assertion.assert(&json_result, |result| println!("{:?}", result))?;
     }
 
     Ok(())
